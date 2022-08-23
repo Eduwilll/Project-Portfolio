e29a7765df69c187051d749aeb5ec8d39dda1871
Eduwill <eduwill100@gmail.com>
2022-08-23 16:39:33 -0300

    Add favicon

--- components/layouts/main.js
+++ components/layouts/main.js
@@ -1,21 +1,32 @@
 import Head from 'next/head';
+import dynamic from 'next/dynamic'
 import NavBar from '../navbar'
 import { Box, Container } from '@chakra-ui/react'
-import VoxelDog from '../voxel-dog';
+import Footer from '../footer'
+import VoxelDogLoader from '../voxel-dog-loader'
 
-const Main = ({children, router}) => {
-    return(
-        <Box as = "main" pb={8}>
+const LazyVoxelDog = dynamic(() => import('../voxel-dog'), {
+    ssr: false,
+    loading: () => <VoxelDogLoader />
+})
+
+const Main = ({ children, router }) => {
+    return (
+        <Box as="main" pb={8}>
             <Head>
                 <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+                <link rel="apple-touch-icon" href="apple-touch-icon.png" />
+                <link rel="shortcut icon" href="/favicon.ico" type="image/x-icon" />
                 <title>Eduardo Fabiricio Goza - Homepage</title>
             </Head>
 
             <NavBar path={router.asPath} />
 
             <Container maxW="container.md" pt={14}>
-                <VoxelDog />
-                {children}    
+                <LazyVoxelDog />
+                {children}
+                <Footer />
+
             </Container>
         </Box>
     )
