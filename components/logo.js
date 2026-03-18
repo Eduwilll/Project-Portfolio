3b22c6cec8897e21fb390a7b44aa53025dcc3158
Eduwilll <eduwill100@gmail.com>
2026-03-17 23:15:09 -0300

    fix: resolve Next.js 16 Link compatibility issues and fix prismjs vulnerability

--- components/logo.js
+++ components/logo.js
@@ -26,19 +26,17 @@ const Logo = () => {
 
     return (
         <Link href="/" scroll={false}>
-            <a>
-                <LogoBox>
-                    <Image src={footPrintImg} width={20} height={20} alt="logo" />
-                    <Text
-                        color={useColorModeValue('gray.800', 'whiteAlpha.900')}
-                        fontFamily='M PLUS Rounded 1c", sans-serif'
-                        fontWeight="bold"
-                        ml={3}
-                    >
-                        Eduardo Fabricio
-                    </Text>
-                </LogoBox>
-            </a>
+            <LogoBox>
+                <Image src={footPrintImg} width={20} height={20} alt="logo" />
+                <Text
+                    color={useColorModeValue('gray.800', 'whiteAlpha.900')}
+                    fontFamily='M PLUS Rounded 1c", sans-serif'
+                    fontWeight="bold"
+                    ml={3}
+                >
+                    Eduardo Fabricio
+                </Text>
+            </LogoBox>
         </Link>
     )
 }
--- components/navbar.js
+++ components/navbar.js
@@ -21,7 +21,7 @@ const LinkItem = ({ href, path, target, children, ...props }) => {
   const active = path === href
   const inactiveColor = useColorModeValue('gray200', 'whiteAlpha.900')
   return (
-        <NextLink href={href} passHref scroll={false}>
+        <NextLink href={href} passHref legacyBehavior scroll={false}>
       <Link
         p={2}
         bg={active ? 'grassTeal' : undefined}
@@ -102,13 +102,13 @@ const Navbar = props => {
                 aria-label="Options"
               />
               <MenuList>
-                <NextLink href="/" passHref>
+                <NextLink href="/" passHref legacyBehavior>
                   <MenuItem as={Link}>About</MenuItem>
                 </NextLink>
-                <NextLink href="/works" passHref>
+                <NextLink href="/works" passHref legacyBehavior>
                   <MenuItem as={Link}>Works</MenuItem>
                 </NextLink>
-                <NextLink href="/posts" passHref>
+                <NextLink href="/posts" passHref legacyBehavior>
                   <MenuItem as={Link}>Posts</MenuItem>
                 </NextLink>
                 <MenuItem
